e2d563c3e207d3ba2c726a1cef6e2a3ee6d21760
murthy.gudipati <mgudipati@saventech.com>
2011-07-06 21:54:00 +0530

    loading dtcc and xignite files into redis

---
+++ load-dtcc.rb
@@ -0,0 +1,92 @@
+require 'csv'
+require 'redis'
+require 'getoptlong'
+require 'json'
+
+# call using "ruby load-dtcc.rb -i<input file>"  
+unless ARGV.length == 1
+  puts "Usage: ruby load-dtcc.rb -i<input file>" 
+  exit  
+end  
+  
+infile = ''
+# specify the options we accept and initialize the option parser  
+opts = GetoptLong.new(  
+  [ "--infile", "-i", GetoptLong::REQUIRED_ARGUMENT ]
+)  
+
+# process the parsed options  
+opts.each do |opt, arg|  
+  case opt  
+    when '--infile'  
+      infile = arg  
+  end  
+end
+
+$redisdb = Redis.new
+$redisdb.select 0
+
+# Process the DTCC basket composition file
+# DTCC file layout is defined as follows:
+# Header record describing the basket information
+# => 01WREI           18383M47200220110624000000950005000000000000000291+0000000000000+0000162471058+0000000003249+0000000004503+0000005000000000000000000+
+# Basket component records
+# => 02AKR            0042391090002011062400000193WREI           18383M472002
+# => 02ALX            0147521090002011062400000013WREI           18383M472002
+# => ...
+if infile && File.exist?(infile)
+  bticker = nil
+  IO.foreach(infile) do |line| 
+    case line[0..1]
+      when '01' # basket header record
+        #Index Receipt Symbol...Trading Symbol
+        bticker = line[2..16].strip
+
+        #Create/Redeem Units per Trade
+        cunit = line[45..52].to_i
+
+        #Total Cash Amount Per Creation Unit...99,999,999,999.99-
+        cash = "#{line[110..120]}.#{line[121..122]}".to_f
+        sign = line[123]
+        if sign == '-' then cash *= -1 end
+
+        #Net Asset Value Per Creation Unit...99,999,999,999.99
+        nav = "#{line[82..92]}.#{line[93..94]}".to_f
+        sign = line[95]
+        if sign == '-' then nav *= -1 end
+
+	      # create a new basket record
+	      $redisdb.hmset  "DTCC:BASKET:#{bticker}",
+	      					      "IndexReceiptSymbol", bticker,
+                        "CreationUnit", cunit,
+                        "TotalCashAmount", cash,
+                        "NAV", nav
+      when '02' # basket component detail record
+        if bticker != nil
+          # create a new basket component
+          hash = Hash.new
+          
+          # Component Symbol...Trading Symbol
+          hash['TickerSymbol'] = line[2..16].strip
+
+          # Component CUSIP...S&P assigned CUSIP
+          hash['CUSIP'] = line[17..25].strip
+
+          #Component Share Qty...99,999,999
+          hash['ShareQuantity '] = line[37..44].to_f
+
+          # store it in a sorted set of basket components
+  	      json = JSON.generate hash
+  	      $redisdb.zadd "DTCC:BASKET:COMPONENTS:#{bticker}", 0, json
+        end
+    end
+  end
+end
+
+=begin rdoc
+ * Name: load-dtcc.rb
+ * Description: Loads the DTCC basket composition file into redis
+ * Author: Murthy Gudipati
+ * Date: 06-Jul-2011
+ * License: Saven Technologies Inc.
+=end
---
+++ load-xignite.rb
@@ -0,0 +1,60 @@
+require 'csv'
+require 'redis'
+require 'getoptlong'
+require 'json'
+
+# call using "ruby load-xignite.rb -i<file1, file2, ...> [-o<file>]"  
+unless ARGV.length >= 1
+  puts "Usage: ruby load-xignite.rb -i<file1, file2, ...> [-o<file>]" 
+  exit  
+end  
+  
+infiles = ''
+outfile = "instruments.xml"
+# specify the options we accept and initialize the option parser  
+opts = GetoptLong.new(  
+  [ "--infile", "-i", GetoptLong::REQUIRED_ARGUMENT ],  
+  [ "--outfile", "-o", GetoptLong::REQUIRED_ARGUMENT ]
+)  
+
+# process the parsed options  
+opts.each do |opt, arg|  
+  case opt  
+    when '--infile'  
+      infiles = arg
+    when '--outfile'  
+      outfile = arg  
+  end  
+end
+
+$redisdb = Redis.new
+$redisdb.select 0
+
+# Process all the xignite master securities files
+infiles.split(',').each do |infile|
+  infile = infile.strip
+  if infile && File.exist?(infile)
+    # Process the xignite master securities file
+    # Xignite master securities file layout is defined as follows:
+    # => " Exchange"," Count"," Records Record Symbol"," Records Record CUSIP"," Records Record CIK"," Records Record ISIN"," Records Record SEDOL"," Records Record Valoren"," Records Record Exchange"," Records Record Name"," Records Record ShortName"," Records Record Issue"," Records Record Sector"," Records Record Industry"," Records Record LastUpdateDate",
+    # => For e.g. NYSE,3581,A,00846U101,0001090872,US00846U1016,2520153,901692,NYSE,"Agilent Technologies Inc.","Agilent Tech Inc","Common Stock",TECHNOLOGY,"Scientific & Technical Instruments",12/3/2005,
+    CSV.foreach(infile, :quote_char => '"', :col_sep =>',', :row_sep => :auto, :headers => true) do |row|
+      ticker = row.field(' Records Record Symbol')
+      $redisdb.hmset  "XIGNITE:SECURITIES:#{ticker}",
+        "TickerSymbol", row.field(' Records Record Symbol'),
+        "CUSIP", row.field(' Records Record CUSIP'),
+        "CIK", row.field(' Records Record CIK'),
+        "ISIN", row.field(' Records Record ISIN'),
+        "SEDOL", row.field(' Records Record SEDOL'),
+        "Valoren", row.field(' Records Record Valoren'),
+        "Exchange", row.field(' Records Record Exchange'),
+        "Name", row.field(' Records Record Name'),
+        "ShortName", row.field(' Records Record ShortName'),
+        "Issue", row.field(' Records Record Issue'),
+        "Sector", row.field(' Records Record Sector'),
+        "Industry", row.field(' Records Record Industry'),
+    end # CSV.foreach
+  else
+    puts "File not found #{infile}"
+  end # if File.exist?(infile)
+end # for each infile  
