9bfe1cbd3c520569a3e5dd0e7529362a1d251e96
murthy.gudipati <mgudipati@saventech.com>
2011-07-13 20:22:37 +0530

    added logger logs

--- gen-tb-xml.rb
+++ gen-tb-xml.rb
@@ -1,5 +1,11 @@
+#!/usr/bin/env ruby
+
 require 'inifile'
+require 'logger'
 require_relative 'common'
+
+$LOG = Logger.new('gen-tb-xml.log')
+$LOG.level = Logger::DEBUG
 
 # Read the INI file
 ini_file = IniFile.load('tbxml.ini')
@@ -58,25 +64,27 @@ case src
     # use xignite files as the source for securities master list
     # Process all the xignite master securities files
     xfiles.split(',').each do |aFile|
-      aFile = aFile.strip
+      aFile.strip!
       if aFile && File.exist?(aFile)
+        $LOG.info("Processing #{aFile}")
         securities = parse_xignite_master_securities_file(aFile)
         create_securities_maps(securities)
       else
-        puts "File not found #{aFile}"
+        $LOG.error("File not found #{aFile}")
       end # if File.exist?(aFile)
     end # for each aFile  
     
   when 'nysegrp'
     # use nyse grp sym file as the source for securities master list
     if nyse_grp_sym_file && File.exist?(nyse_grp_sym_file)
+      $LOG.info("Processing #{nyse_grp_sym_file}")
       securities = parse_nyse_grp_sym_file(nyse_grp_sym_file)
-      #p securities
+      $LOG.debug(securities.inspect)
       create_securities_maps(securities)
-      puts "---num securities by ticker = #{$securities_by_ticker.length}---"
-      puts "---num securities by cusip = #{$securities_by_cusip.length}---"
+      $LOG.info("---num securities by ticker = #{$securities_by_ticker.length}---")
+      $LOG.info("---num securities by cusip = #{$securities_by_cusip.length}---")
     else
-      puts "File not found #{nyse_grp_sym_file}"
+      $LOG.error("File not found #{nyse_grp_sym_file}")
       exit
     end # if File.exist?(nyse_grp_sym_file) 
 end
@@ -86,24 +94,25 @@ create_tbricks_instruments_xml(instrument_reference_data_file, $securities_by_ti
 
 # Process NSCC file
 baskets = Array.new
-missing_cusips = Array.new
 if nscc_file && File.exist?(nscc_file)
+  $LOG.info("Processing #{nscc_file}")
   baskets = parse_nscc_basket_composition_file(nscc_file)
-  #p baskets
   # symbology conversion
   baskets.each do |aBasket|
-    if $securities_by_cusip["#{aBasket.cusip}"] == nil then puts "---missing etf---#{aBasket.tickerSymbol}" end
+    if $securities_by_cusip["#{aBasket.cusip}"] == nil
+      $LOG.info("---missing etf---#{aBasket.tickerSymbol}")
+    end
     aBasket.components.each do |aComponent|
       sec = $securities_by_cusip["#{aComponent.cusip}"]
       if sec != nil
         aComponent.tickerSymbol = sec.tickerSymbol
       else
-        missing_cusips.push(aComponent.cusip)
+        $LOG.info("missing CUSIP: #{aComponent.cusip}")
       end
     end
   end
 else
-  puts "File not found #{nscc_file}"
+  $LOG.error("File not found #{nscc_file}")
   exit
 end # if File.exist?(nscc_file)
 
@@ -112,13 +121,6 @@ create_stub_basket_instruments_xml(basket_stub_instruments_file, baskets)
 
 # build the basket components xml file
 create_basket_components_xml(basket_components_file, baskets)
-
-if missing_cusips.length != 0
-  puts "---missing cusips = #{missing_cusips.length}---"
-  missing_cusips.each do |cusip|
-    puts cusip
-  end
-end
 
 =begin rdoc
  * Name: gen-tb-xml.rb
