4881e6c93a3300dd32769105e36e575982df0203
murthy.gudipati <mgudipati@saventech.com>
2011-08-01 12:35:49 +0530

    dtcc file is processed directly.

--- reports/comp-etfs-report.rb
+++ reports/comp-etfs-report.rb
@@ -1,44 +1,89 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/env ruby
+
 require 'csv'
-require 'redis'
-require 'json'
+require 'getoptlong'
 
-$redisdb = Redis.new
+# call using "ruby comp-etfs-report.rb -i<input file>"  
+unless ARGV.length == 1
+  puts "Usage: ruby comp-etfs-report.rb -i<input file>" 
+  exit  
+end  
+  
+infile = ''
+# specify the options we accept and initialize the option parser  
+opts = GetoptLong.new(  
+  [ "--infile", "-i", GetoptLong::REQUIRED_ARGUMENT ]
+)  
 
-#
-# create a csv file of components and their etf baskets
-# 100, A, ETFA, ETFB, ETFC, ...
-# 10, B, ETFB, ETFC, ...
-# 20, C, ETFA, ETFC, ...
-#
-csv_headers = [
-  "Count",
-  "Component Ticker",
-  "ETF Baskets"
-  ]
-CSV.open("comp-etfs.csv", "wb", :headers => csv_headers, :write_headers => true) do |csv|
-  # obtain all the securities from EDGA exchange db
-  security_keys = $redisdb.keys "EDGA:*"
-  security_keys.each do |security_key|
-    # for each security,
-    security_ticker = $redisdb.hget security_key, "TickerSymbol"
-    security_cusip = $redisdb.hget security_key, "CUSIP"
-
-    # obtain the etf baskets it belongs to...
-    arr = Array.new
-    basket_keys = $redisdb.keys "DTCC:BASKET:*"
-    basket_keys.each do |basket_key|
-      basket_ticker = $redisdb.hget basket_key, "IndexReceiptSymbol"
-      json = $redisdb.hget basket_key, "Components"
-      if json
-        hash = JSON.parse json
-        if hash.key?(security_cusip)
-          arr.push(basket_ticker)
-        end
-      end
-    end
-    
-    # create a csv record
-    csv << [arr.length, security_ticker] + arr
-  end
+# process the parsed options  
+opts.each do |opt, arg|  
+  case opt  
+    when '--infile'  
+      infile = arg  
+  end  
 end
+
+# Process the NSCC basket composition file
+# NSCC file layout is defined as follows:
+# Header record describing the basket information
+# => 01WREI           18383M47200220110624000000950005000000000000000291+0000000000000+0000162471058+0000000003249+0000000004503+0000005000000000000000000+
+# Basket component records
+# => 02AKR            0042391090002011062400000193WREI           18383M472002
+# => 02ALX            0147521090002011062400000013WREI           18383M472002
+# => ...
+def parse_nscc_basket_composition_file( aFile )
+  hash = Hash.new
+  IO.foreach(aFile) do |line|
+    line.chomp!
+    case line[0..1]
+      when '02' #Basket Component Detail
+        #Component Symbol...Trading Symbol
+        comp = line[2..16].strip
+        if comp
+          #Index Receipt Symbol...Trading Symbol
+          etf = line[45..59].strip
+          if etf
+            if !hash.key?(comp)
+              #create an array to hold the etf list for this component
+              hash[comp] = Array.new
+            end
+            #append to the list...
+            hash[comp] << etf
+          end
+        end
+    end
+  end
+
+  return hash
+end
+
+if infile && File.exist?(infile)
+  hash = parse_nscc_basket_composition_file(infile)
+  
+  # create a csv file of components and the etf baskets each is part of
+  headers_a = [
+    "Count",
+    "Component Ticker",
+    "ETF Baskets"
+    ]
+  #
+  # 100,A,ETFA, ETFB, ETFC,....
+  # 11,B,ETFD, EFTF,...
+  # 110,C,ETFA,ETFF,...
+  # ...
+  CSV.open("comp-etfs-report.csv", "wb", :headers => headers_a, :write_headers => true) do |csv|
+    hash.each do |key, value|
+      csv << [value.length, key] + value
+    end
+  end
+else
+  puts "File not found #{infile}"
+end # if File.exist?(infile)
+
+=begin rdoc
+ * Name: comp-etfs-report.rb
+ * Description: Creates a report of security(s) and all the etfs it is part of.
+ * Author: Murthy Gudipati
+ * Date: 12-Jul-2011
+ * License: Saven Technologies Inc.
+=end
