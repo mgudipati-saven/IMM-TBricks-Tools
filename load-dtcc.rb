cb301206f283d8776a4776e30d6c6e4db6835c96
murthy.gudipati <mgudipati@saventech.com>
2011-08-03 20:45:32 +0530

    Refactored: Changed redis db layout to contain DTCC:BASKET:* key set to hold all dtcc baskets and their components in a hash table. Using common parser.

--- load-dtcc.rb
+++ load-dtcc.rb
@@ -27,134 +27,43 @@ $redisdb = Redis.new
 $redisdb.select 0
 
 #
-# Process the NSCC basket composition file
-# NSCC file layout is defined as follows:
-# Header record describing the basket information
-# => 01WREI           18383M47200220110624000000950005000000000000000291+0000000000000+0000162471058+0000000003249+0000000004503+0000005000000000000000000+
-# Basket component records
-# => 02AKR            0042391090002011062400000193WREI           18383M472002
-# => 02ALX            0147521090002011062400000013WREI           18383M472002
-# => ...
 # Redis layout is as follows:
 # Key => DTCC:BASKET:#{Index Receipt CUSIP}
-# Value => Hashtable {"CUSIP" => "123456789, ""TickerSymbol" => "ETFA", "CreationUnit" => "50000", ...}
-#
-# Key => DTCC:COMP:#{CUSIP}
-# Value => Hashtable {"CUSIP" => "123456789, ""TickerSymbol" => "IBM", "ETFA" => "500", "ETFB" => "100"}
+# Value => Hashtable {"IndexReceiptSymbol" => "SPY", "CreationUnit" => "50000", "Components" => json object of components hash}
 #
 if infile && File.exist?(infile)
-  numrec = 0
-
-  IO.foreach(infile) do |line|
-    line.chomp!
-    case line[0..1]
-      when '01' #Basket Header
-        numrec += 1
-
-        #Index Receipt Symbol...Trading Symbol
-        sym = line[2..16].strip
-
-        #Index Receipt CUSIP...S&P assigned CUSIP
-        cusip = line[17..25].strip
-
-        #Create a component record
-        $redisdb.hmset "DTCC:COMP:#{cusip}", "CUSIP", cusip, "TickerSymbol", sym
-
-        #Create a redis key using the cusip and store the record
-        key = "DTCC:BASKET:#{cusip}"
-        $redisdb.hmset key, "CUSIP", cusip, "TickerSymbol", sym
-	      
-        #When Issued Indicator...0 = Regular Way 1 = When Issued
-        $redisdb.hset key, "WhenIssuedIndicator", line[26]
-
-        #Foreign Indicator...0 = Domestic 1 = Foreign
-        $redisdb.hset key, "ForeignIndicator", line[27]
-
-        #Exchange Indicator...0 = NYSE 1 = AMEX 2 = Other
-        $redisdb.hset key, "ExchangeIndicator", line[28]
-
-        #Portfolio Trade Date...CCYYMMDD
-        $redisdb.hset key, "TradeDate", line[29..36]
-
-        #Component Count...99,999,999
-        $redisdb.hset key, "ComponentCount", line[37..44].to_i
-
-        #Create/Redeem Units per Trade...99,999,999
-        $redisdb.hset key, "CreationUnitsPerTrade", line[45..52].to_i
-
-        #Estimated T-1 Cash Amount Per Creation Unit...999,999,999,999.99-
-        val = "#{line[53..64]}.#{line[65..66]}".to_f
-        sign = line[67]
-        if sign == '-' then val *= -1 end
-        $redisdb.hset key, "EstimatedT1CashAmountPerCreationUnit", val
-        
-        #Estimated T-1 Cash Per Index Receipt...99,999,999,999.99
-        val = "#{line[68..78]}.#{line[79..80]}".to_f
-        sign = line[81]
-        if sign == '-' then val *= -1 end
-        $redisdb.hset key, "EstimatedT1CashPerIndexReceipt", val
-
-        #Net Asset Value Per Creation Unit...99,999,999,999.99
-        val = "#{line[82..92]}.#{line[93..94]}".to_f
-        sign = line[95]
-        if sign == '-' then val *= -1 end
-        $redisdb.hset key, "NAVPerCreationUnit", val
-
-        #Net Asset Value Per Index Receipt...99,999,999,999.99
-        val = "#{line[96..106]}.#{line[107..108]}".to_f
-        sign = line[109]
-        if sign == '-' then val *= -1 end
-        $redisdb.hset key, "NAVPerIndexReceipt", val
-
-        #Total Cash Amount Per Creation Unit...99,999,999,999.99-
-        val = "#{line[110..120]}.#{line[121..122]}".to_f
-        sign = line[123]
-        if sign == '-' then val *= -1 end
-        $redisdb.hset key, "TotalCashAmount", val
-
-        #Total Shares Outstanding Per ETF...999,999,999,999
-        $redisdb.hset key, "TotalSharesOutstanding", line[124..135].to_i
-
-        #Dividend Amount Per Index Receipt...99,999,999,999.99
-        val = "#{line[136..146]}.#{line[147..148]}".to_f
-        sign = line[149]
-        if sign == '-' then val *= -1 end        
-        $redisdb.hset key, "DividendAmount", val
-
-        #Cash / Security Indicator...  1 = Cash only 2 = Cash or components other – components only
-        $redisdb.hset key, "CashIndicator", line[150]
-      when '02' #Basket Component Detail
-        numrec += 1
-
-        #Component Symbol...Trading Symbol
-        csym = line[2..16].strip
-
-        #Component CUSIP...S&P assigned CUSIP
-        ccusip = line[17..25].strip
-
-        #Create a component record
-        key = "DTCC:COMP:#{ccusip}"
-        $redisdb.hsetnx key, "CUSIP", ccusip
-        $redisdb.hsetnx key, "TickerSymbol", csym
-        
-        #Component Share Qty...99,999,999
-        qty = line[37..44].to_f
-
-        #Index Receipt CUSIP...S&P assigned CUSIP
-        bcusip = line[60..68].strip
-        
-        #Update the component record with the share qty for the basket
-        $redisdb.hset key, bcusip, qty
-      when '09' #File Trailer
-        numrec += 1
-
-        # Record Count...99,999,999 Includes Records 01, 02, 09
-        reccnt = line[37..44].to_i
-        if numrec != reccnt
-          puts "Error in DTCC File: records found:#{numrec} != records reported:#{reccnt}"
-        end
-    end
-  end                    
+  baskets_a = parse_nscc_basket_composition_file(infile)
+  baskets_a.each do |aBasket| 
+    # create a new basket record
+    $redisdb.hmset  "DTCC:BASKET:#{aBasket.cusip}",
+    					      "IndexReceiptCUSIP", aBasket.cusip,
+    					      "IndexReceiptSymbol", aBasket.tickerSymbol,
+    					      "WhenIssuedIndicator", aBasket.whenIssuedIndicator,
+    					      "ForeignIndicator", aBasket.foreignIndicator,
+    					      "ExchangeIndicator", aBasket.exchangeIndicator,
+    					      "TradeDate", aBasket.tradeDate,
+    					      "ComponentCount", aBasket.componentCount,
+                    "CreationUnitsPerTrade", aBasket.creationUnitsPerTrade,
+    					      "EstimatedT1CashAmountPerCreationUnit", aBasket.estimatedT1CashAmountPerCreationUnit,
+    					      "EstimatedT1CashPerIndexReceipt", aBasket.estimatedT1CashPerIndexReceipt,
+    					      "NAVPerCreationUnit", aBasket.navPerCreationUnit,
+    					      "NAVPerIndexReceipt", aBasket.navPerIndexReceipt,
+    					      "TotalCashAmount", aBasket.totalCashAmount,
+    					      "TotalSharesOutstanding", aBasket.totalSharesOutstanding,
+    					      "DividendAmount", aBasket.dividendAmount,
+                    "CashIndicator", aBasket.cashIndicator
+                    
+    # store the basket components
+    if aBasket.components
+      hash = Hash.new
+      aBasket.components.each do |key, value|
+        #  cusip => share qty
+        hash[key] = value.shareQuantity
+      end
+      json = JSON.generate hash
+      $redisdb.hset "DTCC:BASKET:#{aBasket.cusip}", "Components", json
+    end                          
+  end
 else
   puts "File not found #{infile}"
 end # if File.exist?(infile)
